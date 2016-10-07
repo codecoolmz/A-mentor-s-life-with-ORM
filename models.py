8e1b1da7c146620d0e0b2e06930fd9de15a75eb7
codecoolmz <vagasiferi4ever@gmail.com>
2016-10-07 20:21:13 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,41 @@
+from peewee import *
+
+# Configure your database connection here
+# database name = should be your username on your laptop
+# database user = should be your username on your laptop
+db = PostgresqlDatabase('orm_exc', user='mz')
+
+
+class BaseModel(Model):
+    """A base model that will use our Postgresql database"""
+    class Meta:
+        database = db
+
+
+class CodecoolClass(BaseModel):
+    location = CharField()
+    year = IntegerField()
+
+    def mentors(self):
+        return self.ment
+
+    def students(self):
+        return self.st
+
+
+class Person(BaseModel):
+    first_name = CharField()
+    last_name = CharField()
+    year_of_birth = DateField()
+    gender = CharField()
+
+
+class Student(Person):
+    knowledge_level = IntegerField()
+    energy_level = IntegerField()
+    codecool_class = ForeignKeyField(CodecoolClass, related_name='st')
+
+
+class Mentor(Person):
+    nickname = CharField()
+    codecool_class = ForeignKeyField(CodecoolClass, related_name='ment')
